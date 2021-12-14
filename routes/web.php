7e4988781b4efbbc556a28195e6ffc0b533d289e
Aji Siwi <182410101122@students.unej.ac.id>
2021-12-15 06:09:07 +0700

    update scm

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\admin\DashboardController;
-use App\Http\Controllers\admin\MaterialController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\MaterialController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Middleware\RegisteredAuth;
